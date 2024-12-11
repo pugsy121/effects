6443d001b99cb1f1b9140c6a235c0fc7f5fdafd9
pugsy121 <70470246+pugsy121@users.noreply.github.com>
2024-12-11 18:22:50 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('playButton').addEventListener('click', function() {
-    const videoPlayer = document.getElementById('videoPlayer');
-    videoPlayer.style.display = 'block'; // Show the video player
-    videoPlayer.play(); // Play the video
-});
